7a795ff43ee12f134eb41e1f58afba86070f7574
Lol8005 <ekhoos3141@gmail.com>
2025-02-24 21:05:49 +0800

    Add files via upload

--- practical1Q4/add_subject.php
+++ practical1Q4/add_subject.php
@@ -1,5 +1,3 @@
-<!-- Havent finish-->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,21 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Subject</title>
 </head>
+
+<?php 
+    if(isset($_POST["code"])){
+        require 'connection.php';
+
+        $sql = "INSERT INTO subjects (code, title, credit, yearOfStudy) VALUES (?,?,?,?)";
+        $stmt= $conn->prepare($sql);
+        $stmt->execute([$_POST["code"], $_POST["title"], $_POST["credit"], $_POST["year"]]);
+
+        header('Location: show_subject.php');
+        die();
+    }
+?>
+
 <body>
     <h1><u>Add New Subject</u></h1>
-    <br>
-    <form>
-        <label for="code">Subject Code:</label>
-        <input type="text" name="code">
+    <form method="POST">
+        <label for="code">Subject Code:</label><br>
+        <input type="text" name="code"><br><br>
 
-        <label for="title">Title: </label>
-        <input type="text" name="title">
+        <label for="title">Title: </label><br>
+        <input type="text" name="title"><br><br>
 
-        <label for="credit">Credit: </label>
-        <input type="text" name="credit">
+        <label for="credit">Credit: </label><br>
+        <input type="text" name="credit"><br><br>
 
-        <label for="year">Year of Study</label>
-        <input type="text" name="year">
+        <label for="year">Year of Study</label><br>
+        <input type="text" name="year"><br><br>
 
         <button type="submit">Add</button>
     </form>
