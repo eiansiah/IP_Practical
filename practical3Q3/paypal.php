c33cd3a9bd3b7afd161da7127f238f9d671e5901
Lol8005 <ekhoos3141@gmail.com>
2025-03-25 16:27:26 +0800

    Create paypal.php

---
+++ practical3Q3/paypal.php
@@ -0,0 +1,15 @@
+<?php
+interface IPayPal {
+    public function getCreditCardNo();
+    public function getCustomerName();
+    public function getCardExpMonth();
+    public function getCardExpYear();
+    public function getCardCVVNo();
+    public function getAmount();
+    public function setCreditCardNo($creditCardNo);
+    public function setCustomerName($customerName);
+    public function setCardExpMonth($cardExpMonth);
+    public function setCardExpYear($cardExpYear);
+    public function setCardCVVNo($cardCVVNo);
+    public function setAmount($amount);
+}
