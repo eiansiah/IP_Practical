88a3a7a42cbabeb58d65c85bd45599a2f8f74e78
Eddead <simhk-wp22@student.tarc.edu.my>
2025-03-27 22:03:25 +0800

    Update

--- Eddie/Practical3/PartB/StripeInterface.php
+++ Eddie/Practical3/PartB/StripeInterface.php
@@ -1,5 +1,5 @@
 <?php
-    interface Stripe {
+    interface StripeInterface {
         public function getCustCardNo();
         public function getCardOwnerName();
         public function getCardExpMonthDate();
@@ -11,3 +11,53 @@
         public function setCVVNo($cvvNo);
         public function setTotalAmount($totalAmount);
     }
+    
+class Stripe implements StripeInterface{
+    private $cardNo;
+    private $exp;
+    private $name;
+    private $cvv;
+    private $amount;
+
+    public function getCustCardNo(){
+        return $this->cardNo;
+    }
+
+    public function getCardOwnerName(){
+        return $this->name;
+    }
+
+    public function getCardExpMonthDate(){
+        return $this->exp;
+    }
+
+    public function getCVVNo(){
+        return $this->cvv;
+    }
+
+    public function getTotalAmount(){
+        return $this->amount;
+    }
+
+    public function setCustCardNo($custCardNo){
+        $this->cardNo = $custCardNo;
+    }
+
+    public function setCardOwnerName($cardOwnerName){
+        $this->name = $cardOwnerName;
+    }
+
+    public function setCardExpMonthDate($cardExpMonthDate){
+        $this->exp = $cardExpMonthDate;
+
+        return $this->exp;
+    }
+
+    public function setCVVNo($cvvNo){
+        $this->cvv = $cvvNo;
+    }
+
+    public function setTotalAmount($totalAmount){
+        $this->amount = $totalAmount;
+    }
+}
